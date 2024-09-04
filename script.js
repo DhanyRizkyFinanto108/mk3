0a4a0448cbbf822f9ee3867649e0e338e18dd5f9
DhanyRizkyFinanto108 <541231048@student.smktelkom-pwt.sch.id>
2024-09-04 13:16:46 +0700

    add fitur login

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const correctUsername = "nyye";
+    const correctPassword = "nyye";
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if(username === correctUsername && password === correctPassword){
+        alert("Login Berhasil");
+        window.location.href = "index.html";
+
+    }else{
+        alert("Username atau password salah");
+    };
+});
